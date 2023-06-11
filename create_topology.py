dd7c36af6bc731f9188ebe2114752a448f4f4b43
Adam Niemiec <72918433+aniemiec862@users.noreply.github.com>
2023-06-11 15:38:39 +0200

    Typo

--- create_topology.py
+++ create_topology.py
@@ -13,11 +13,11 @@ router_role_id = client.create_device_role(name="router_role")
 host_role_id = client.create_device_role(name="host_role")
 
 
-#build the topology
+#create devices
 router_id = client.create_device(name="router", type_id=router_device_type, role_id=router_role_id, site_id=site_id)
 host_id = client.create_device(name="host", type_id=host_device_type, role_id=host_role_id, site_id=site_id)
 
-#that how you connect 2 devices
+#that is how you connect 2 devices
 interface_id = client.create_interface(name="int1", device_id=router_id)
 interface2_id = client.create_interface(name="int2", device_id=host_id)
 cable_id = client.create_cable(interface_id, interface2_id)
