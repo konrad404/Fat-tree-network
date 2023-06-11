c7baf77497dcc80599dc66028c01c926937f1721
Adam <adamniemiec862@gmail.com>
2023-06-11 15:25:22 +0200

    Add netbox client and auth mechanism

---
+++ create_topology.py
@@ -0,0 +1,23 @@
+from netbox_client import NetboxClient
+
+#create netbox client
+client = NetboxClient()
+client.auth()
+
+#setup project
+site_id = client.create_site(name="site")
+manufacturer_id = client.create_manufacturer(name="cisco")
+router_device_type = client.create_device_type(name="router", manufacturer_id=manufacturer_id, model_name="cool_model")
+host_device_type = client.create_device_type(name="host", manufacturer_id=manufacturer_id, model_name="cool_model")
+router_role_id = client.create_device_role(name="router_role")
+host_role_id = client.create_device_role(name="host_role")
+
+
+#build the topology
+router_id = client.create_device(name="router", type_id=router_device_type, role_id=router_role_id, site_id=site_id)
+host_id = client.create_device(name="host", type_id=host_device_type, role_id=host_role_id, site_id=site_id)
+
+#that how you connect 2 devices
+interface_id = client.create_interface(name="int1", device_id=router_id)
+interface2_id = client.create_interface(name="int2", device_id=host_id)
+cable_id = client.create_cable(interface_id, interface2_id)
---
+++ netbox_client.py
@@ -0,0 +1,135 @@
+import requests
+
+NETBOX_HOST = "http://localhost:8000"
+USERNAME = "a"
+PASSWORD = "123"
+
+
+class NetboxClient:
+    def __init__(self):
+        self.headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json; indent=4"
+        }
+
+    def auth(self):
+        body = {
+            "username": USERNAME,
+            "password": PASSWORD
+        }
+
+        response = self.send_request("POST", f"{NETBOX_HOST}/api/users/tokens/provision/", body)
+        api_token = response.json()['display']
+        self.headers["Authorization"] = f"Token {api_token}"
+
+    def create_site(self, name):
+        site = {
+            "name": name,
+            "slug": name
+        }
+
+        response = self.send_request("POST", f"{NETBOX_HOST}/api/dcim/sites/", body=site)
+
+        device_id = response.json()["id"]
+        print(f"Site {name} with id {device_id} created")
+        return device_id
+
+    def create_manufacturer(self, name):
+        manufacturer = {
+            "name": name,
+            "slug": name
+        }
+
+        response = self.send_request("POST", f"{NETBOX_HOST}/api/dcim/manufacturers/", body=manufacturer)
+
+        manufacturer_id = response.json()["id"]
+        print(f"Manufacturer {name} with id {manufacturer_id} created")
+        return manufacturer_id
+
+    def create_device_type(self, name, manufacturer_id, model_name):
+        device_type = {
+            "name": name,
+            "manufacturer": manufacturer_id,
+            "model": model_name,
+            "slug": name
+        }
+
+        response = self.send_request("POST", f"{NETBOX_HOST}/api/dcim/device-types/", body=device_type)
+
+        device_type_id = response.json()["id"]
+        print(f"Device type {name} with id {device_type_id} created")
+        return device_type_id
+
+    def create_device_role(self, name):
+        device_role = {
+            "name": name,
+            "slug": name
+        }
+
+        response = self.send_request("POST", f"{NETBOX_HOST}/api/dcim/device-roles/", body=device_role)
+
+        device_role_id = response.json()["id"]
+        print(f"Device role {name} with id {device_role_id} created")
+        return device_role_id
+
+    def create_device(self, name, type_id, role_id, site_id):
+        device = {
+            "name": name,
+            "device_type": type_id,
+            "device_role": role_id,
+            "site": site_id,
+        }
+
+        response = self.send_request("POST", f"{NETBOX_HOST}/api/dcim/devices/", body=device)
+
+        device_id = response.json()["id"]
+        print(f"Device {name} with id {device_id} created")
+        return device_id
+
+    def create_interface(self, name, device_id):
+        interface = {
+            "name": name,
+            "type": "1000base-t",
+            "device": device_id,
+        }
+
+        response = self.send_request("POST", f"{NETBOX_HOST}/api/dcim/interfaces/", body=interface)
+
+        interface_id = response.json()["id"]
+        print(f"Interface {name} with id {interface_id} created")
+        return interface_id
+
+    def create_cable(self, int1_id, int2_id):
+        cable = {
+            "a_terminations": [
+                {
+                    "object_type": "dcim.interface",
+                    "object_id": int1_id,
+                },
+            ],
+            "b_terminations": [
+                {
+                    "object_type": "dcim.interface",
+                    "object_id": int2_id,
+                },
+            ],
+            "status": "connected",
+        }
+        response = self.send_request("POST", f"{NETBOX_HOST}/api/dcim/cables/", body=cable)
+
+        cable_id = response.json()["id"]
+        print(f"Cable for interfaces {int1_id} and {int2_id} with id {cable_id} created")
+        return cable_id
+
+    def send_request(self, method, url, body):
+        if method == "POST":
+            response = requests.post(url, headers=self.headers, json=body)
+        elif method == "DELETE":
+            response = requests.delete(url, headers=self.headers)
+        else:
+            raise ValueError(f"Unsupported method: {method}")
+
+        if response.status_code >= 400:
+            print(f"Error {response.status_code} while sending {method} request to {url}: {response.text}")
+            return
+        return response
