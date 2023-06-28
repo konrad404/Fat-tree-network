07d226f0c5c3cec45679bd44e2be0dcbc30b0cfc
Pawel Hanusik <39835401+pawelhanusik@users.noreply.github.com>
2023-06-28 14:38:48 +0200

    Load distances from json file

--- distances.py
+++ distances.py
@@ -1,4 +1,14 @@
+import json, sys
+
 class Distances:
     core_to_aggregation = 100
     aggregation_to_edge = 20
     edge_to_host = 10
+
+def loadDistances():
+    with open('distances.json') as json_file:
+        data = json.load(json_file)
+        for key in data:
+            setattr(Distances, key, data[key])
+
+loadDistances()
